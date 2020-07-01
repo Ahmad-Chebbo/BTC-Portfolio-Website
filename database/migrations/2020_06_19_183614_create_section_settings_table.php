afa80b0e667c23abee69665cb99b5377d483c2b8
AhmadShebbo <41969693+InfernoX0@users.noreply.github.com>
2020-07-01 13:06:41 +0300

    Update 2020_06_19_183614_create_section_settings_table.php

--- database/migrations/2020_06_19_183614_create_section_settings_table.php
+++ database/migrations/2020_06_19_183614_create_section_settings_table.php
@@ -15,23 +15,6 @@ class CreateSectionSettingsTable extends Migration
     {
         Schema::create('section_settings', function (Blueprint $table) {
             $table->id();
-            // $table->string('portfolio')->nullable();
-            // $table->string('p_sub')->nullable();
-            // $table->boolean('p_en')->default(true);
-            // $table->string('about')->nullable();
-            // $table->string('a_sub')->nullable();
-            // $table->boolean('a_en')->default(true);
-            // $table->string('experience')->nullable();
-            // $table->string('ex_sub')->nullable();
-            // $table->boolean('ex_en')->default(true);
-            // $table->string('education')->nullable();
-            // $table->string('ed_sub')->nullable();
-            // $table->boolean('ed_en')->default(true);
-            // $table->string('counter')->nullable();
-            // $table->boolean('c_en')->default(true);
-            // $table->string('quote')->nullable();
-            // $table->boolean('q_en')->default(true);
-
             $table->string('section')->nullable();
             $table->string('header')->nullable();
             $table->text('subtitle')->nullable();
